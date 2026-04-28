6747ffc83aab2285d668dfb588d0016a6a5aea83
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-04-28 10:58:16 +0530

    terraform aws roboshop module

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z08496491D9BJEMR7DC94"
+}
+
+variable "zone_name" {
+  default = "prudhvisai.space"
+}
+
+variable "component" {
+
+}
+
+variable "rule_priority" {
+
+}
