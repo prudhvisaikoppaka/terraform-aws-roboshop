df7e5642d5f39fc17abd52a641b81e16139fe2b7
Prudhvi Sai Koppaka <prudhvisaikoppaka99@gmail.com>
2026-05-09 21:52:29 +0530

    terraform infra roboshop module

--- data.tf
+++ data.tf
@@ -30,9 +30,9 @@ data "aws_ssm_parameter" "catalogue_sg_id" {
   name = "/${var.project}/${var.environment}/catalogue_sg_id"
 }
 
-# data "aws_ssm_parameter" "sg_id" {
-#   name = "/${var.project}/${var.environment}/${var.component}_sg_id"
-# }
+data "aws_ssm_parameter" "sg_id" {
+  name = "/${var.project}/${var.environment}/${var.component}_sg_id"
+}
 
 # data "aws_ssm_parameter" "backend_alb_listener_arn" {
 #   name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
